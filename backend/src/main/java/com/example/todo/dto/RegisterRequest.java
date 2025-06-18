f5ef1113c624ef6a956c2f3542d4144d745eb667
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-06-18 16:00:42 +0530

    Update RegisterRequest.java

--- backend/src/main/java/com/example/todo/dto/RegisterRequest.java
+++ backend/src/main/java/com/example/todo/dto/RegisterRequest.java
@@ -1,5 +1,9 @@
+package com.example.todo.dto;
+
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
-    private String email;
+    private String username;
     private String password;
-    private String name;
 }
