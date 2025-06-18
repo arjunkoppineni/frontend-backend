374bb906228ee76814b26b39ae169f9c146089de
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-06-18 15:12:19 +0530

    Create TaskController.java

---
+++ backend/src/main/java/com/example/todo/controller/TaskController.java
@@ -0,0 +1,27 @@
+@RestController
+@RequestMapping("/api/tasks")
+@PreAuthorize("hasRole('USER')")
+public class TaskController {
+
+    @Autowired private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks(Authentication auth) {
+        return taskService.getTasksByUser(auth.getName());
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task, Authentication auth) {
+        return taskService.saveTask(task, auth.getName());
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
