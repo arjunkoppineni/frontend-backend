8540c22a1bfc787d180d4a85ea51b24b06722ef4
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-06-20 10:46:27 +0530

    Update User.java

--- backend/src/main/java/com/example/todo/entity/User.java
+++ backend/src/main/java/com/example/todo/entity/User.java
@@ -17,9 +17,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
